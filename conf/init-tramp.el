094f0eb99ab6da41a49b10a89eec5cebb99d167d
Tomo Matsumoto <tomoyuki28jp@yahoo.co.jp>
2009-01-04 05:03:43 +0900

    fix tramp-file-p func in init-tramp.el

--- conf/init-tramp.el
+++ conf/init-tramp.el
@@ -1,13 +1,8 @@
-;;; -*- Coding: utf-8 -*-
 ;;; init-tramp.el
 
 (require 'tramp)
 (setq tramp-default-method "ssh")
 (setq tramp-auto-save-directory "/tmp")
 
-; バッファ名からtramp経由で読み込んだファイルかどうか判別
 (defun tramp-file-p ()
-  (string-match "^/ssh:"
-                (if (null buffer-file-name)
-                    ""
-                  buffer-file-name)))
+  (string-match "^/ssh:" (or buffer-file-name "")))
