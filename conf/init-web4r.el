920a24cc1dc9963fcfac102ea07847b1964bcd10
Tomo Matsumoto <tomoyuki28jp@yahoo.co.jp>
2009-01-08 08:25:46 +0900

    Refactored init-web4r.el

--- conf/init-web4r.el
+++ conf/init-web4r.el
@@ -1,13 +1,26 @@
 ;;; init-web4r.el
 ;;; web4r: common lisp web application framework
 
-(defun delete-fasl ()
+(defvar *web4r-src-dir* (expand-file-name "~/src/lisp/web4r/"))
+
+(defvar *sbcl-cache-dir*
+  "/var/cache/common-lisp-controller/1000/sbcl/local")
+
+(defvar *web4r-cache-dir* (concat *sbcl-cache-dir* *web4r-src-dir*))
+
+(defun exec-concat-cmd (&rest args)
+  (shell-command-to-string (apply #'concat args)))
+
+(defun rm-dir  (dir)
+  (exec-concat-cmd "rm -rf " dir))
+
+(defun rm-fasl (dir)
+  (exec-concat-cmd "find " dir " -type f -name '*.fasl' -exec rm {} \\;"))
+
+(defun remove-fasl ()
   (interactive)
-  (shell-command-to-string
-   (concat
-    "for i in "
-    "`find " (expand-file-name "~/src/lisp/web4r/") " -name \"*.fasl\"`;"
-    " do rm $i; done;")))
+  (rm-dir *web4r-cache-dir*)
+  (rm-fasl *web4r-src-dir*))
 
 (setq auto-mode-alist
       (append '(("\\.shtml$" . lisp-mode)) auto-mode-alist))
