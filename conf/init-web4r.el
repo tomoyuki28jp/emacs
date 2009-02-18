282d3dfb49e714423fad52c995e47166803c7fed
Tomo Matsumoto <tomoyuki28jp@yahoo.co.jp>
2009-02-18 12:43:19 +0900

    Added web4r submodule

--- conf/init-web4r.el
+++ conf/init-web4r.el
@@ -1,28 +1,4 @@
 ;;; init-web4r.el
-;;; web4r: common lisp web application framework
 
-(defvar *web4r-src-dir* (expand-file-name "~/src/lisp/web4r/"))
-
-(defvar *sbcl-cache-dir*
-  "/var/cache/common-lisp-controller/1000/sbcl/local")
-
-(defvar *web4r-cache-dir* (concat *sbcl-cache-dir* *web4r-src-dir*))
-
-(defun exec-concat-cmd (&rest args)
-  (shell-command-to-string (apply #'concat args)))
-
-(defun rm-dir  (dir)
-  (exec-concat-cmd "rm -rf " dir))
-
-(defun rm-fasl (dir)
-  (exec-concat-cmd "find " dir " -type f -name '*.fasl' -exec rm {} \\;"))
-
-(defun remove-fasl ()
-  (interactive)
-  (rm-dir *web4r-cache-dir*)
-  (rm-fasl *web4r-src-dir*))
-
-(global-set-key "\C-xrf" 'remove-fasl)
-
-(setq auto-mode-alist
-      (append '(("\\.shtml$" . lisp-mode)) auto-mode-alist))
+(add-to-load-path "~/.emacs.d/elisp/web4r/")
+(load "web4r")
