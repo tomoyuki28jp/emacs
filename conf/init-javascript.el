6107d8c2f2e4674e4f518c1091a83a00a24e04b4
Tomo Matsumoto <tomoyuki28jp@yahoo.co.jp>
2009-04-21 03:54:15 +0900

    Updated init-javascript.el to use javascript.el instead of js2.el

--- conf/init-javascript.el
+++ conf/init-javascript.el
@@ -1,4 +1,4 @@
 ;;; init-javascript.el
 
-(autoload 'js2-mode "js2" nil t)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . javascript-mode))
+(autoload 'javascript-mode "javascript" nil t)
