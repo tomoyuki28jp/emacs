3aca4463a532c48cde52bc05aa1472c1aa309b3e
Tomo Matsumoto <tomo@X300.(none)>
2009-05-04 00:53:37 +0900

    Updated init-color.el for urxvt with 256 colors

--- conf/init-color.el
+++ conf/init-color.el
@@ -1,13 +1,9 @@
 ;;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
-(if (eq window-system 'x)
-    (progn
-      (add-to-load-path "~/.emacs.d/elisp/color-theme/")
-      (require 'color-theme)
-      (color-theme-initialize)
-      (color-theme-billw))  
-  (progn
-    (set-face-foreground 'font-lock-comment-face "darkolivegreen3")
-    (set-face-background 'highlight "black")
-    (set-face-foreground 'highlight "white")
-    ))
+(add-to-load-path "~/.emacs.d/elisp/color-theme/")
+(require 'color-theme)
+(color-theme-initialize)
+(color-theme-dark-laptop)
+(set-face-foreground 'font-lock-comment-face "yellow")
+(set-face-background 'highlight "brightblack")
+(set-face-foreground 'highlight "white")
