4a7865e653bc34b0918950f685f0a8b7ad7ade71
Tomo Matsumoto <tomo@X300.(none)>
2009-09-12 03:50:02 +0900

    Updated to use Osaka-font for emacs-snapshot-gtk

--- conf/init-font.el
+++ conf/init-font.el
@@ -2,12 +2,13 @@
 
 (when (>= emacs-major-version 23)
   (add-to-list 'default-frame-alist
-               '(font . "Bitstream Vera Sans Mono-9"))
-  (set-default-font "Bitstream Vera Sans Mono-9")
+               '(font . "Osaka－等幅-9"))
+  (set-default-font "Osaka－等幅-9")
   (set-fontset-font "fontset-default"
                     'japanese-jisx0208
-                    '("VL Gothic" . "unicode-bmp")))
-(set-default-font "-*-fixed-medium-r-normal--14-*-*-*-*-*-*-*")
+                    '("Osaka－等幅-9" . "unicode-bmp")))
+;(set-default-font "-*-fixed-medium-r-normal--14-*-*-*-*-*-*-*")
+(set-default-font "-apple-Osaka－等幅-normal-normal-normal-*-9-*-*-*-d-0-iso10646-1")
 
 ;; Bug for emacs-snapshot-gtk with Ubuntu 8.10
 ;; https://bugs.launchpad.net/ubuntu/+source/emacs-snapshot/+bug/291399
