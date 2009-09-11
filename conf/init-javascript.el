0532bfe627a69a70872fe30de05a9ac7efa5d9fd
Tomo Matsumoto <tomo@X300.(none)>
2009-09-12 03:55:04 +0900

    Added (add-hook 'javascript-mode-hook (lambda () (setq indent-tabs-mode nil)))

--- conf/init-javascript.el
+++ conf/init-javascript.el
@@ -2,3 +2,5 @@
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . javascript-mode))
 (autoload 'javascript-mode "javascript" nil t)
+(add-hook 'javascript-mode-hook
+          (lambda () (setq indent-tabs-mode nil)))
