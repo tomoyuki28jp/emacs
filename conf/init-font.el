4ea1018d07dec217941aab50856e260c4d911cc7
Tomo Matsumoto <tomo@X300.(none)>
2009-09-12 03:54:39 +0900

    Removed unneeded code

--- conf/init-font.el
+++ conf/init-font.el
@@ -9,7 +9,3 @@
                     '("Osaka－等幅-9" . "unicode-bmp")))
 ;(set-default-font "-*-fixed-medium-r-normal--14-*-*-*-*-*-*-*")
 (set-default-font "-apple-Osaka－等幅-normal-normal-normal-*-9-*-*-*-d-0-iso10646-1")
-
-;; Bug for emacs-snapshot-gtk with Ubuntu 8.10
-;; https://bugs.launchpad.net/ubuntu/+source/emacs-snapshot/+bug/291399
-(set-frame-parameter nil 'font-backend '(xft x))
