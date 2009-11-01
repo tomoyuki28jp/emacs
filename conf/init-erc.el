28e03b4e02a5b5ea59f562de0ae8e31f050d255b
Tomo Matsumoto <tomo@X300.(none)>
2009-11-01 14:08:36 +0900

    Added "(erc-spelling-mode 1)"

--- conf/init-erc.el
+++ conf/init-erc.el
@@ -7,12 +7,15 @@
        :nick "tomoyuki28jp"
        :password tiarra-pass))
 
+(erc-spelling-mode 1)
+
 (setq erc-current-nick-highlight-type 'nick)
 ;(setq erc-keywords '("tomoyuki28jp"))
 (setq erc-track-exclude-types '("JOIN" "PART" "QUIT" "NICK" "MODE"))
 (setq erc-track-use-faces t)
 ;(setq erc-track-faces-priority-list '(erc-current-nick-face erc-keyword-face))
 (setq erc-track-priority-faces-only 'all)
+
 (add-hook 'erc-text-matched-hook
           (lambda (match-type nickuserhost message)
             (let* ((channel (car (split-string (buffer-name) "@")))
@@ -22,4 +25,5 @@
                       "^*** Users on #"))
                    (matched (loop for keyword in exclude-keywords
                                   when (string-match keyword message) return t)))
-              (unless matched (stumpish (concat "erc: " channel))))))
+              (unless matched
+                (stumpish (concat "erc: " channel))))))
