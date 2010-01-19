cbe9b1bb0ab4a09756078a0e649b7fa64a66a27f
Tomo Matsumoto <tomo@X300.(none)>
2010-01-20 03:16:43 +0900

    Added custom-set-faces for nxhtml-mode

--- conf/init-html.el
+++ conf/init-html.el
@@ -5,3 +5,11 @@
       '(lambda () (setq-default
                    tab-width 4
                    indent-tabs-mode nil)))
+
+(custom-set-faces
+ '(mumamo-background-chunk-major ((t (:background "dark"))))
+ '(mumamo-background-chunk-submode ((t (:background "dark"))))
+ '(mumamo-background-chunk-submode1 ((t (:background "dark"))))
+ '(mumamo-background-chunk-submode2 ((t (:background "dark"))))
+ '(mumamo-background-chunk-submode3 ((t (:background "dark"))))
+ '(mumamo-background-chunk-submode4 ((t (:background "dark")))))
