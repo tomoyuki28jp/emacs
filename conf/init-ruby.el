229901c32e6aa1313cf68e7a3cc5eeb5afe154cf
Tomo Matsumoto <tomo@X300.(none)>
2010-01-26 02:01:50 +0900

    Added elisp/ruby/*.el and updated to use them

--- conf/init-ruby.el
+++ conf/init-ruby.el
@@ -1,2 +1,6 @@
 ;;; init-ruby.el
+(add-to-load-path "~/.emacs.d/elisp/ruby/")
 (require 'ruby-mode)
+(require 'ruby-electric)
+(add-hook 'ruby-mode-hook
+          '(lambda () (ruby-electric-mode t)))
