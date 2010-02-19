8475a35b850c54264158b43292bd8b96dd5d277b
松本 智行 <tomo@em114-48-143-149.pool.e-mobile.ne.jp>
2010-02-20 04:08:14 +0900

    Changed the values of set-frame-parameter

--- conf/init-frame.el
+++ conf/init-frame.el
@@ -1,7 +1,10 @@
 ;;; init-frame.el
 
-(set-frame-parameter nil 'alpha '(85 75))
-(set-frame-parameter nil 'fullscreen 'fullheight)
-(set-frame-parameter nil 'width 180)
-(set-frame-parameter nil 'left 0)
+; doesn't work with SizeUp
+;(set-frame-parameter nil 'fullscreen 'fullboth)
 
+(set-frame-parameter nil 'alpha '(85 75))
+(set-frame-parameter nil 'height 50)
+(set-frame-parameter nil 'width 180)
+(set-frame-parameter nil 'top 0)
+(set-frame-parameter nil 'left 0)
