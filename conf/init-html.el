1687b8e36f4a70c1732dc76113c800fceee1cf60
松本 智行 <tomo@macbook.local>
2010-02-25 19:32:28 +0900

    Updated nxhtml-mode configurations

--- conf/init-html.el
+++ conf/init-html.el
@@ -1,13 +1,14 @@
 ;;; init-html.el
 
+(load "~/.emacs.d/elisp/nxhtml/autostart.el")
+
 (add-hook 'nxhtml-mode-hook
       '(lambda () (setq indent-tabs-mode nil tab-width 4)))
 
-(unless (eq system-type 'darwin)
-  (custom-set-faces
-   '(mumamo-background-chunk-major    ((t (:background "dark"))))
-   '(mumamo-background-chunk-submode  ((t (:background "dark"))))
-   '(mumamo-background-chunk-submode1 ((t (:background "dark"))))
-   '(mumamo-background-chunk-submode2 ((t (:background "dark"))))
-   '(mumamo-background-chunk-submode3 ((t (:background "dark"))))
-   '(mumamo-background-chunk-submode4 ((t (:background "dark"))))))
+(custom-set-faces
+ '(mumamo-background-chunk-major    ((t (:background "dark"))))
+ '(mumamo-background-chunk-submode  ((t (:background "dark"))))
+ '(mumamo-background-chunk-submode1 ((t (:background "dark"))))
+ '(mumamo-background-chunk-submode2 ((t (:background "dark"))))
+ '(mumamo-background-chunk-submode3 ((t (:background "dark"))))
+ '(mumamo-background-chunk-submode4 ((t (:background "dark")))))
