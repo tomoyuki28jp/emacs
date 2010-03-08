5d09364c829481fcc2c46f878cd5b7f084f40a83
Tomo Matsumoto <tomo@X300.(none)>
2010-03-08 14:41:46 +0900

    Comment out (setq w3m-default-display-inline-images t)

--- conf/init-w3m.el
+++ conf/init-w3m.el
@@ -2,7 +2,7 @@
 
 (add-to-load-path "~/.emacs.d/elisp/emacs-w3m")
 (require 'w3m)
-;(setq w3m-default-display-inline-images t)
+(setq w3m-default-display-inline-images t)
 (setq browse-url-browser-function 'w3m-browse-url)
 (setq w3m-use-cookies t)
 ;(setq w3m-pop-up-frames t)
