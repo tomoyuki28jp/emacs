1fc347fad9297d326936e0fac1112a9c5626e789
松本 智行 <tomo@macbook.local>
2010-03-17 16:48:07 +0900

    Modified tramp setting for Mac

--- conf/init-tramp.el
+++ conf/init-tramp.el
@@ -1,8 +1,22 @@
 ;;; init-tramp.el
 
+(when (eq system-type 'darwin)
+  (add-to-load-path "~/.emacs.d/elisp/tramp-2.1.9/lisp"))
 (require 'tramp)
 (setq tramp-default-method "ssh")
 (setq tramp-auto-save-directory "/tmp")
+(setq tramp-shell-prompt-pattern "^.*[#$%>] *")
+
+(setq tramp-methods
+      (cons
+       '("my-sudo" (tramp-login-program "env SHELL=/bin/sh sudo")
+         (tramp-login-args (("-u" "%u") ("-s") ("-H") ("-p" "Password:")))
+         (tramp-remote-sh "/bin/sh")
+         (tramp-copy-program nil)
+         (tramp-copy-args nil)
+         (tramp-copy-keep-date nil)
+         (tramp-password-end-of-line nil))
+       tramp-methods))
 
 (defun tramp-file-p ()
   (string-match "^/ssh:" (or buffer-file-name "")))
