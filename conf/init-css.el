8d375c8f38282cdacf88df380065520956f8cab7
松本 智行 <tomo@em114-48-182-254.pool.e-mobile.ne.jp>
2010-03-18 16:31:24 +0900

    s/cssm-indent-level/css-indent-offset/

--- conf/init-css.el
+++ conf/init-css.el
@@ -1,7 +1,7 @@
 ;;; init-css.el
 
 (require 'css-mode)
-(setq cssm-indent-level 2)
+(setq css-indent-offset 2)
 (setq cssm-newline-before-closing-bracket t)
 (setq cssm-indent-function #'cssm-c-style-indenter)
 (setq cssm-mirror-mode t)
