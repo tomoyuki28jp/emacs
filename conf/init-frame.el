e1f627b2b8f5e83c504e27f94e13a8e8561163fe
Tomo Matsumoto <tomo@x300.(none)>
2010-10-04 18:36:01 +0900

    Updated the frame setting

--- conf/init-frame.el
+++ conf/init-frame.el
@@ -1,17 +1,16 @@
 ;;; init-frame.el
 
-; doesn't work with SizeUp
-;(set-frame-parameter nil 'fullscreen 'fullboth)
+(if (eq system-type 'darwin)
+  (set-frame-parameter nil 'alpha '(100 80))
+  (set-frame-parameter nil 'height 50)
+  (set-frame-parameter nil 'width 180)
+  (set-frame-parameter nil 'top 0)
+  (set-frame-parameter nil 'left 0)
 
-(set-frame-parameter nil 'alpha '(100 80))
-(set-frame-parameter nil 'height 50)
-(set-frame-parameter nil 'width 180)
-(set-frame-parameter nil 'top 0)
-(set-frame-parameter nil 'left 0)
+  (defun toggle-fullscreen ()
+    (interactive)
+    (set-frame-parameter
+     nil 'fullscreen
+     (unless (frame-parameter nil 'fullscreen) 'fullboth)))
 
-(defun toggle-fullscreen ()
-  (interactive)
-  (set-frame-parameter nil 'fullscreen
-                       (unless (frame-parameter nil 'fullscreen) 'fullboth)))
-
-(global-set-key "\C-cf" 'toggle-fullscreen)
+  (global-set-key "\C-cf" 'toggle-fullscreen))
