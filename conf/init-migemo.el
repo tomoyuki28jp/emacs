f5b87a50b0a9c70040cdc2576d9d9d400d88e2b0
Tomo Matsumoto <tomo@x300.(none)>
2010-10-10 21:40:31 +0900

    use cmigemo on linux machines

--- conf/init-migemo.el
+++ conf/init-migemo.el
@@ -1,15 +1,21 @@
 ;;; init-migemo.el
 
-(when (eq system-type 'darwin)
-  (add-to-load-path "/Applications/Emacs.app/Contents/Resources/share/emacs/site-lisp/")
-  (setq migemo-command "migemo")
-  (setq migemo-options '("-t" "emacs"))
-  (setq migemo-dictionary "/Applications/Emacs.app/Contents/Resources/share/migemo/migemo-dict")
-  (setq migemo-user-dictionary nil)
-  (setq migemo-regex-dictionary nil)
-  (setenv "RUBYLIB" "/Applications/Emacs.app/Contents/Resources/lib/ruby/site_ruby/"))
-
-(require 'migemo)
-
-; turn off migemo when emacs starts
-(migemo-toggle-isearch-enable)
+(if (progn
+      (eq system-type 'darwin)
+      (add-to-load-path "/Applications/Emacs.app/Contents/Resources/share/emacs/site-lisp/")
+      (setq migemo-command "migemo")
+      (setq migemo-options '("-t" "emacs"))
+      (setq migemo-dictionary "/Applications/Emacs.app/Contents/Resources/share/migemo/migemo-dict")
+      (setq migemo-user-dictionary nil)
+      (setq migemo-regex-dictionary nil)
+      (setenv "RUBYLIB" "/Applications/Emacs.app/Contents/Resources/lib/ruby/site_ruby/")
+      (require 'migemo))
+    (progn
+      (setq migemo-command "cmigemo")
+      (setq migemo-options '("-q" "--emacs"))
+      (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+      (setq migemo-user-dictionary nil)
+      (setq migemo-regex-dictionary nil)
+      (setq migemo-coding-system 'utf-8-unix)
+      (load-library "migemo")
+      (migemo-init)))
