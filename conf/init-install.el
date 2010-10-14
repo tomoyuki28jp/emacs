0dca168f108cc38bc71a991706d435e092cd51c7
Tomo Matsumoto <tomo@x300.(none)>
2010-10-14 15:53:17 +0900

    disable auto-install

--- conf/init-install.el
+++ conf/init-install.el
@@ -3,7 +3,7 @@
 (require 'install-elisp)
 (setq install-elisp-repository-directory "~/.emacs.d/elisp/")
 
-(require 'auto-install)
-(setq auto-install-directory "~/.emacs.d/elisp/")
-(auto-install-update-emacswiki-package-name t)
-(auto-install-compatibility-setup)
+;(require 'auto-install)
+;(setq auto-install-directory "~/.emacs.d/elisp/")
+;(auto-install-update-emacswiki-package-name t)
+;(auto-install-compatibility-setup)
